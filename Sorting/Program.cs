7ff6ce92882e609dc1b97e7ec631a2c1113e3941
Kirill <Kirill@LOL-FACTORY>
2019-07-03 13:06:40 +0300

    Add project files.

---
+++ Sorting/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    public class Program
+    {
+        public static long Init(int range)
+        {
+            int[,] m = new int[range, range];
+
+            var timer = System.Diagnostics.Stopwatch.StartNew();
+            for (int i = 0; i < range; i++)
+            {
+                for (int k = 0; k < range; k++)
+                {
+                    m[i, k] = 1;
+                }
+            }
+            timer.Stop();
+
+
+            Console.WriteLine(timer.ElapsedMilliseconds);
+            return timer.ElapsedMilliseconds;
+        }
+
+        public static long InitDiagonal(int range)
+        {
+            int[,] m = new int[range, range];
+
+            var timer = System.Diagnostics.Stopwatch.StartNew();
+            for (int i = 0; i < range; i++)
+            {
+                m[i, i] = 2;
+                for (int k = 0; k < range; k++)
+                {
+                    m[i, k] = m[k, i] = 1;
+                }
+            }
+            timer.Stop();
+
+            Console.WriteLine(timer.ElapsedMilliseconds);
+            return timer.ElapsedMilliseconds;
+        }
+
+        static void Main(string[] args)
+        {
+            int range = 50;
+
+            // loop
+            // Timer start
+            List<long> time = new List<long>();
+            for (int i = 0; i < range; i++)
+            {
+                time.Add( Init(5000));
+            }
+            // Timer stop
+            Console.WriteLine(time.Average());
+            /* range:
+             * 500 - 284,162 
+             * 50  - 282,54 
+             * 10  - 320,5 (280)
+             * 100 - 279,85*/
+            Console.WriteLine("___________");
+
+            List<long> time2 = new List<long>();
+            for (int i = 0; i < range; i++)
+            {
+                time2.Add(InitDiagonal(5000));
+            }
+            // Timer stop
+            Console.WriteLine(time2.Average());
+            /* range:
+             
+             * 50  - 996 (1034)
+             * 10 -  1008*/
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ SortingTestProject/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Sorting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SortingTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void InitTestMethod()
+        {
+            Sorting.Program.Init(5000);
+        }
+
+        [TestMethod]
+        public void InitDiagonalTestMethod()
+        {
+            Sorting.Program.InitDiagonal(5000);
+        }
+
+    }
+}
